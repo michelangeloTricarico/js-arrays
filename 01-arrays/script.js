1bc1e2aab580ecade78cf179cf2b859c88ea770f
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-03 21:27:35 +0100

    teacher replacement

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,9 +11,11 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3]; //for 4th theacher in the list index == 3 because index start to 0
-console.log(fourthTeacher) // to print variable
+console.log(fourthTeacher); // to print variable
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = "Patrick" // Carlo is the 5th teacher in the list previously, new value is Patrick
+console.log(teachers) // To check that Carlo is out of list and Patrick is in 5th position
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
