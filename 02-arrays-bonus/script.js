a710d92ff57ad740ecdb579301e0fcbc322e41cc
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-03 22:38:33 +0100

    Fabio in the list

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -39,7 +39,17 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const Fabio_index = teachers.indexOf("Fabio"); // chech if Fabio is in the list with the index
+if (Fabio_index != -1){ // if a value different from -1 is the result Fabio is in the list
+  const isFabioPresent = true;
+  console.log(isFabioPresent)
+}
+else{ // if index is -1 Fabio isn't in the list
+  const isFabioPresent = false;
+  console.log(isFabioPresent)
+}
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
