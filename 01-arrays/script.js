39ef8a4d9438a168bef28441b525950a95ec0663
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-03 21:22:26 +0100

    fourthTeacher

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,7 +10,8 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3]; //for 4th theacher in the list index == 3 because index start to 0
+console.log(fourthTeacher) // to print variable
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
